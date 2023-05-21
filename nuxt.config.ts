8687523e9b116eb230eb2581a713db3f11031ca0
t000uf <taikimushi@gmail.com>
2023-05-21 13:12:15 +0900

    Add nuxt-microcms-module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,11 @@ export default defineNuxtConfig({
     strict: true,
     typeCheck: true
   },
-})
+
+  modules: ["nuxt-microcms-module"],
+  microCMS: {
+    serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: process.env.MICROCMS_API_KEY,
+  },
+
+});
