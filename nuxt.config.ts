445ac62d89e654306416560f209e4f44ae948e0d
t000uf <taikimushi@gmail.com>
2023-05-21 15:23:31 +0900

    Add @nuxtjs/google-adsense

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,14 +9,25 @@ export default defineNuxtConfig({
       },
     },
   },
-  
+
   typescript: {
     shim: false,
     strict: true,
     typeCheck: true
   },
 
-  modules: ["nuxt-microcms-module"],
+  modules: [
+    "nuxt-microcms-module",
+    ['@nuxtjs/google-adsense', {
+      id: 'ca-pub-7931455677905492',
+      pageLevelAds: true,
+      analyticsUacct: process.env.GA_TRACKING_ID,
+      analyticsDomainName: 'gadgetgurashi.com',
+      onPageLoad: true,
+      test: true, //開発終了時に無効化
+    }],
+],
+
   microCMS: {
     serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
     apiKey: process.env.MICROCMS_API_KEY,
