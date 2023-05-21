d5a414876fa35f96e629e7d6844b8c52f9602039
t000uf <taikimushi@gmail.com>
2023-05-21 15:24:36 +0900

    Add フッターとサイドバーの追加

---
+++ components/footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer class="foot">
+    <nuxt-link to="#">page top</nuxt-link>
+      <div class="footnav">
+        <ul>
+          <li><nuxt-link to="/">home</nuxt-link></li>
+          <li><nuxt-link to="/article/about">about</nuxt-link></li>
+        </ul>
+      </div>
+    <small>© 2022 gadgetgurashi</small>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+
+.foot{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: $bg-gray;
+}
+
+.foot a{
+  margin: 0 10px;
+  color: $text-color;
+}
+
+.footnav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.footnav ul {
+  padding-left: 0;
+}
+
+.footnav ul li {
+  display: inline-block;
+}
+
+.footnav ul li a{
+  color: $text-color;
+}
+
+</style>
---
+++ components/side.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup></script>
+twitter-follow-button.value = '';
+<template>
+  <div class="side">
+    <!--<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-lang="ja" data-show-count="true">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <a href="https://twitter.com/gadgetgurashi?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-lang="ja" data-show-count="false">Follow @gadgetgurashi</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>-->
+    <adsbygoogle 
+     :ad-slot="'7173714878'" 
+     :ad-format="'auto'"
+     :ad-style="{ display: 'block' }"
+     />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+@media screen and (min-width:1025px) {
+  
+  .side{
+    position: sticky;
+    top: 90px;
+    width: 250px;
+    height: 85vh;
+    box-sizing: border-box;
+    border-radius: 10px;
+    margin: 30px 0;
+    padding: 10px;
+    background-color: $bg-lightgray;
+  }
+}
+
+</style>
