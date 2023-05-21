93d03aad9168d312c97d6c3022e3fd5d546aeb93
t000uf <taikimushi@gmail.com>
2023-05-21 14:04:54 +0900

    Add vue3-smooth-scrollの追加

---
+++ plugins/vue3-smooth-scroll.ts
@@ -0,0 +1,7 @@
+// Use vue3-smooth-scroll
+import { defineNuxtPlugin } from "#app"
+import VueSmoothScroll from "vue3-smooth-scroll"
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.use(VueSmoothScroll)
+})
