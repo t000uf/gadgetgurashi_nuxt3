8798d269d1c779df34cdcf3361192734f74f131b
t000uf <taikimushi@gmail.com>
2026-03-30 17:01:09 +0900

    Feat: Headプロパティ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,38 @@ export default defineNuxtConfig({
     typeCheck: true,
   },
 
+  app: {
+    head: {
+      title: 'がじぇっとぐらし！',
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: http://ogp.me/ns#',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'オタクがガジェットやその他話題について書き散らします' },
+        { property: 'og:site_name', content: 'がじぇっとぐらし！' },
+        { property: 'og:type', content: 'blog' },
+        { property: 'og:url', content: 'https://www.gadgetgurashi.com' },
+        { property: 'og:title', content: 'がじぇっとぐらし！' },
+        {
+          property: 'og:description',
+          content: 'オタクがガジェットやその他話題について書き散らします',
+        },
+        {
+          property: 'og:image',
+          content: 'https://gadgetgurashi.com/_nuxt/img/20210824_022917.47cb9f8.jpg',
+        },
+        { property: 'og:locale', content: 'ja_JP' },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:site', content: '@gadgetgurashi' },
+        { name: 'twitter:site', content: '@nzn_gadget' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+
   modules: [
     'nuxt-microcms-module',
     [
