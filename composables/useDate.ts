ecdb288422994deb5fcf6e3f97f7c66def4b60fd
t000uf <taikimushi@gmail.com>
2026-03-30 16:59:27 +0900

    Fix: 型エラー解消

--- composables/useDate.ts
+++ composables/useDate.ts
@@ -1,9 +1,11 @@
 export const useDate = () => {
-  const format = (date: string) =>
-    new Date(date).toLocaleDateString('ja', {
+  const format = (date: string | undefined) => {
+    if (!date) return '';
+    return new Date(date).toLocaleDateString('ja', {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
     });
+  };
   return { format };
 };
