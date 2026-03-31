cf72a5172300ca4c3360d48b8a3661d3f461d384
t000uf <taikimushi@gmail.com>
2026-03-31 14:49:34 +0900

    Remove: VueSmoothScroll削除

--- plugins/vue3-smooth-scroll.ts
+++
@@ -1,7 +0,0 @@
-// Use vue3-smooth-scroll
-import { defineNuxtPlugin } from '#app';
-import VueSmoothScroll from 'vue3-smooth-scroll';
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueSmoothScroll);
-});
